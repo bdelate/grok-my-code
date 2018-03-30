beb32830c3277ee5846f2d2388c840f458362b18
bdelate <brendon.delate@gmail.com>
2018-03-30 05:16:13 -0400

    Created portfolio page along with cards

--- codeabstraction/core/urls.py
+++ codeabstraction/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Home
+from .views import Home, Portfolio
 
 app_name = 'core'
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('portfolio/', Portfolio.as_view(), name='portfolio'),
 ]
--- codeabstraction/core/views.py
+++ codeabstraction/core/views.py
@@ -5,3 +5,9 @@ class Home(TemplateView):
 
     template_name = 'core/index.html'
     extra_context = {'active_link': 'about'}
+
+
+class Portfolio(TemplateView):
+
+    template_name = 'core/portfolio.html'
+    extra_context = {'active_link': 'portfolio'}
