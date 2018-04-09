04d1ca0e19de09777bbea750eb20078e574d72c1
bdelate <brendon.delate@gmail.com>
2018-04-09 11:05:30 -0400

    Created Diversify Portfolio project page

--- codeabstraction/core/urls.py
+++ codeabstraction/core/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from .views import Home, Portfolio, WhoKnows
+from .views import Home, Portfolio, DiversifyPortfolio, WhoKnows
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', Home.as_view(), name='home'),
-    path('portfolio/', Portfolio.as_view(), name='portfolio'),
-    path('portfolio/whoknows/', WhoKnows.as_view(), name='whoknows'),
+    path('',
+         Home.as_view(),
+         name='home'),
+    path('portfolio/',
+         Portfolio.as_view(),
+         name='portfolio'),
+    path('portfolio/diversifyportfolio/',
+         DiversifyPortfolio.as_view(),
+         name='diversifyportfolio'),
+    path('portfolio/whoknows/',
+         WhoKnows.as_view(),
+         name='whoknows'),
 ]
