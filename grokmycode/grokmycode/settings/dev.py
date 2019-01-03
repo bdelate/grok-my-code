3a72986759b18453cf5ea3c0a022fd6e24bb152f
bdelate <brendon.delate@gmail.com>
2019-01-03 10:44:59 +0200

    Changed to use sqlite db instead of postgres in dev settings

--- grokmycode/grokmycode/settings/dev.py
+++ grokmycode/grokmycode/settings/dev.py
@@ -12,11 +12,7 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'db',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
